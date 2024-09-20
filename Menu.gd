93c18eb1f53dbb04c07d808b4dd2a7b830b9fb74
Yaheeha-Baheeha <yahalb44@kingshigh.school.nz>
2024-09-20 17:29:59 +1200

    Just added assets for future use
    :moyai:

--- Menu.gd
+++ Menu.gd
@@ -1,8 +1,31 @@
 extends Control
 
+#Main menu
+#No more explanation I barely coded this
+
+var diff = 0
+
+func _on_option_button_item_selected(index):
+	if index == 0:
+		diff = -1
+	elif index == 1:
+		diff = 0
+	elif index == 2:
+		diff = 1
+
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://scenes/world.tscn")
-	$AudioStreamPlayer.stop()
+	if diff == 0:
+		get_tree().change_scene_to_file("res://scenes/world.tscn")
+		Engine.time_scale = 1
+		$AudioStreamPlayer.stop()
+	elif diff == 1:
+		get_tree().change_scene_to_file("res://scenes/worldz-hard.tscn")
+		Engine.time_scale = 1
+		$AudioStreamPlayer.stop()
+	elif diff == -1:
+		get_tree().change_scene_to_file("res://scenes/world-ez.tscn")
+		Engine.time_scale = 1
+		$AudioStreamPlayer.stop()		
 
 
 func _on_options_pressed():
@@ -11,3 +34,6 @@ func _on_options_pressed():
 
 func _on_quit_pressed():
 	get_tree().quit()
+
+
+
